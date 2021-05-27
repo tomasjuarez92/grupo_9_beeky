bd4952bd3330903c6557a8b635a55b0f45ed22c2
tomasjuarez92 <tomasjuarez92@gmail.com>
2021-05-27 00:19:58 -0300

    Inicio Sprint 2 - Home, Header y Footer

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+const path = require ('path')
 const express=require("express");
 const app=express();
-const path=require("path");
+
 app.use(express.static(path.resolve(__dirname,"./public")));
+app.listen(3050,() => console.log('Terminal Corriendo'))
+
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname,"./views/home.html")))
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")))
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname,"./views/login.html")))
+app.get('/productCart', (req,res) => res.sendFile(path.resolve(__dirname,"./views/productCart.html")))
+app.get('/productDetail', (req,res) => res.sendFile(path.resolve(__dirname,"./views/productDetail.html")))
