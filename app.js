05167f53d70f6a84e28d351e086ab23acc8e9465
tomasjuarez92 <tomasjuarez92@gmail.com>
2021-05-29 19:07:05 -0300

    Subo modificaciones en la estructura

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app=express();
 app.use(express.static(path.resolve(__dirname,"./public")));
 app.listen(3050,() => console.log('Terminal Corriendo'))
 
-app.get('/', (req,res) => res.sendFile(path.resolve(__dirname,"./views/home.html")))
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname,"./views/index.html")))
 app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")))
 app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname,"./views/login.html")))
 app.get('/productCart', (req,res) => res.sendFile(path.resolve(__dirname,"./views/productCart.html")))
